959a2a072cb2d5a81a30619198a380b01fcdad8d
Syed Mohammed <mohsyd90@gmail.com>
2021-05-18 01:38:55 -0700

    feat(derby-service): debry service for getting player names and leagues

---
+++ src/main/java/com/uc/infotechandmobapps/debrynamesservice/dao/repo/DerbyNamesRepository.java
@@ -0,0 +1,9 @@
+package com.uc.infotechandmobapps.debrynamesservice.dao.repo;
+
+import com.uc.infotechandmobapps.debrynamesservice.dao.entity.DerbyNames;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DerbyNamesRepository extends JpaRepository<DerbyNames, String> {
+}
---
+++ src/test/java/com/uc/infotechandmobapps/debrynamesservice/DerbyNamesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uc.infotechandmobapps.debrynamesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DerbyNamesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
